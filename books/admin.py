3bb30abfee9e58d05b57a921b0dd6a6757c88066
eliana_lipkova <lipkovaeliana@gmail.com>
2026-03-25 14:43:42 +0100

    add Django admin setup, models (Book, Author, Series, Genre) and configuration

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Book, Author,Series, Genre
 
-# Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'serie', 'order')
+    filter_horizontal = ('author', 'genre')
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class SeriesAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Series, SeriesAdmin)
+admin.site.register(Genre, GenreAdmin)
--- books/models.py
+++ books/models.py
@@ -2,17 +2,23 @@ from django.db import models
 
 
 class Author(models.Model):
-    author_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.author_name
+        return self.name
 
 
 class Series(models.Model):
-    serie_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.serie_name
+        return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Book(models.Model):
@@ -20,6 +26,7 @@ class Book(models.Model):
     serie = models.ForeignKey(Series, on_delete=models.SET_NULL, null=True, blank=True)
     order = models.IntegerField(null=True, blank=True)
     author = models.ManyToManyField(Author)
+    genre = models.ManyToManyField(Genre, blank=True)
 
     def __str__(self):
         return self.name
