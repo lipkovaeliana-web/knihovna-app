765f062dec584f274a52835317d4b25d26204a68
eliana_lipkova <lipkovaeliana@gmail.com>
2026-03-30 11:15:15 +0200

    add language and read status fields to Book model and admin

--- books/admin.py
+++ books/admin.py
@@ -3,7 +3,9 @@ from .models import Book, Author,Series, Genre
 
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('name', 'serie', 'order')
+    list_display = ('name','language', 'is_read', 'serie', 'order')
+    list_flter = ('language', 'is_read', 'genre', 'serie')
+    search_fields = ('name',)
     filter_horizontal = ('author', 'genre')
 
 class AuthorAdmin(admin.ModelAdmin):
--- books/models.py
+++ books/models.py
@@ -22,11 +22,31 @@ class Genre(models.Model):
 
 
 class Book(models.Model):
+    LANGUAGE_CHOICES = [
+        ('cs', 'Čeština'),
+        ('en', 'Angličtina'),
+    ]
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES,
+        max_length=2,
+        default='cs',
+    )
+    is_read = models.BooleanField(default=False)
+
     name = models.CharField(max_length=500)
     serie = models.ForeignKey(Series, on_delete=models.SET_NULL, null=True, blank=True)
     order = models.IntegerField(null=True, blank=True)
     author = models.ManyToManyField(Author)
     genre = models.ManyToManyField(Genre, blank=True)
 
+
+for book in Book.objects.all():
+    if book.language == 'CZ':
+        book.language = 'cs'
+    elif book.language == 'AJ':
+        book.language = 'en'
+    book.save()
+
     def __str__(self):
         return self.name
+
