65fb61536e7b32d10da97ecd1af00028f92262a1
eliana_lipkova <lipkovaeliana@gmail.com>
2026-03-31 12:14:32 +0200

    soubor utils.py

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   useEffect(() => {
     async function fetchBooks() {
-      const response = await fetch("http://127.0.0.1:8000/api/books/");
+      const response = await fetch("http://localhost:8000/api/books/");
       const data = await response.json();
       console.log(data);
       setBooks(data.results);
